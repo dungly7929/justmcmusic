7b3dfe9d9c6883b164c8e1a1cb5125724a338161
Danny Ly <danks76429@gmail.com>
2023-03-13 17:20:02 -0400

    inital commit

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,13 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from "react-router-dom";
+
+const DetailsHeader = ({artistId, artistData, songData}) => (
+
+
+    <div className="relative w-full flex flex-col">
+        <div className="w-full">
+
+        </div>
+      </div>
+  )
 
 export default DetailsHeader;
