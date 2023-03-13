529892cf39a6ca0bda7370d31239229e7e6c8463
Danny Ly <danks76429@gmail.com>
2023-03-13 19:30:38 -0400

    convert shazamcore work with v2

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const shazamCoreApi = createApi({
   reducerPath: 'ShazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
     prepareHeaders: (headers) => {
       headers.set('X-RapidAPI-KEY', 'f7e6f71808msh411a51addcf0e99p1e81cfjsndf3132583c28');
 
@@ -11,17 +11,21 @@ export const shazamCoreApi = createApi({
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getTopCharts: builder.query({ query: () => 
+      'v1/charts/world' }),
     getSongdetails: builder.query({ query: (songid) => 
-      `/tracks/details?track_id=${songid}` }),
+      `v1/tracks/details?track_id=${songid}` }),
     getSongRelated: builder.query({ query: (songid) => 
-      `/tracks/related?track_id=${songid}`}),
+      `v1/tracks/related?track_id=${songid}`}),
+      getArtistDetails: builder.query({ query: (artistId) => 
+        `v2/artists/details?artist_id=${artistId}` }),
   }),
 });
-
 
 export const { 
   useGetTopChartsQuery, 
   useGetSongdetailsQuery,
-  useGetSongRelatedQuery, 
+  useGetSongRelatedQuery,
+  useGetArtistDetailsQuery,
+
 } = shazamCoreApi;
