7529d01f3a6b7a5678465dca6a9b8b511c630dae
Danny Ly <danks76429@gmail.com>
2023-03-13 17:19:46 -0400

    first commit

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'ShazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam.p.rapidapi.com',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-KEY', 'f7e6f71808msh411a51addcf0e99p1e81cfjsndf3132583c28');
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/track' }),
+    getSongdetails: builder.query({ query: (songid) => `/songs/get-details?key=${songid}&locale=en-US` }),
+  }),
+});
+
+export const { 
+  useGetTopChartsQuery, 
+  useGetSongdetailsQuery 
+} = shazamCoreApi;
