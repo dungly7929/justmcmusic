05e598c5f0988c93a08bd0f8aa5fdad8f14bd9a7
Danny Ly <danks76429@gmail.com>
2023-03-13 17:32:56 -0400

    DetaailsHeader add stylelist to the page

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -4,8 +4,17 @@ const DetailsHeader = ({artistId, artistData, songData}) => (
 
 
     <div className="relative w-full flex flex-col">
-        <div className="w-full">
-
+        <div className="w-full bg-gradient-to-l from-transparent to-black
+        sm:h-48 h-28"/>
+        <div className="absolute inset-0 flex items-center">
+            <img 
+            src={artistId ? artistData?.artists[artistId].attributes?.
+            artwork?.url.replace('{w}','500').replace('{h}','500') : 
+            songData?.images?.coverart} 
+            alt="art"
+            className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-
+            border-2 shadow-xl shadow-black"
+            />
         </div>
       </div>
   )
